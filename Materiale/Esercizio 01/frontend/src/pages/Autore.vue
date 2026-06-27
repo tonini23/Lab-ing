a554b6032235d832fde9761496335b11b97207fa
Andrea Tonini <toniniandri23@yahoo.it>
2026-06-27 22:21:46 +0200

    Fix visualizzazione autore

--- Materiale/Esercizio 01/frontend/src/pages/Autore.vue
+++ Materiale/Esercizio 01/frontend/src/pages/Autore.vue
@@ -6,27 +6,34 @@ import axios from "axios"
 export default defineComponent({
   data() {
     return {
-      autore: null as Autore | null
+      autore: null as Autore | null,
+      articoliAutore: [] as any[]
     }
   },
   methods: {
     getAutore() {
       axios.get(`/api/autore/${this.$route.params.idAutore}`)
         .then(response => this.autore = response.data[0])
+    },
+    getArticoliAutore() {
+      axios.get(`/api/articoli/autore/${this.$route.params.idAutore}`)
+        .then(response => this.articoliAutore = response.data)
     }
   },
   mounted() {
     this.getAutore()
+    this.getArticoliAutore()
   }
 })
 </script>
 
 <template>
     <template v-if="autore">
-        <h2>{{autore.nome}}</h2>
-        <article>
-            <h3>{{autore.username}}</h3>
-            <p>{{autore.argomenti}}</p>
+        <h2>{{autore.nome}} - {{autore.username}}</h2>
+        <article v-for="articolo in articoliAutore" :key="articolo.idarticolo">
+            <img :src="'/img/' + articolo.imgarticolo" alt="immagine articolo" />
+            <p>{{autore.nome}} {{articolo.dataarticolo.slice(0, 10)}}</p>
+            <p>{{articolo.anteprimaarticolo}}</p>
         </article>
     </template>
 </template>
