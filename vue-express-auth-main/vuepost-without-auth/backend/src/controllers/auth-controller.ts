e77331357fe0da2b69f1f6ba7af4351e95078abb
Andrea Tonini <toniniandri23@yahoo.it>
2026-06-29 20:23:58 +0200

    Implementate le funzioni register, login, getUser, set e unsetUser

--- vue-express-auth-main/vuepost-without-auth/backend/src/controllers/auth-controller.ts
+++ vue-express-auth-main/vuepost-without-auth/backend/src/controllers/auth-controller.ts
@@ -1,23 +1,34 @@
 import { Request, Response } from "express"
-import { User } from "../utils/auth"
+import { setUser, User } from "../utils/auth"
 import { connection } from "../utils/db"
+import bycrypt from "bcrypt"
 
 export const register = async (req: Request, res: Response) => {
   // Estrae username e password dal body della richiesta
+
   const { username, password } = req.body
 
+  const [rows] = await connection.execute(`SELECT username FROM users WHERE 
+  username=?`, [username])
+
+  if(Array.isArray(rows) && rows.length > 0){
+    res.status(400).send("Username già in uso")
+    return
+  }
+
+  const passwordHash = await bycrypt.hash(password, 10)
   // Inserisce l'utente nel database
-  await connection.execute("INSERT INTO users (username, password) VALUES (?, ?)", [
-    username,
-    password,
-  ])
+  await connection.execute("INSERT INTO users (username, password) VALUES (?, ?)", 
+    [username, passwordHash,])
 
   // Estrae i dati del nuovo utente
-  const [results] = await connection.execute(
+  const [users] = await connection.execute(
     "SELECT id, username, role FROM users WHERE username=?",
     [username]
   )
-  const newUser = (results as User[])[0]
+  const userData = (users as User[])[0]
+
+  setUser(req, res, userData)
 
   res.json({ message: "Registrazione effettuata con successo" })
 }
@@ -27,10 +38,24 @@ export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body
 
   // Esegue la query al database per ottenere i dati dell'utente in base allo username
-  const [results] = await connection.execute(
-    "SELECT id, username, password, role FROM users WHERE username=?",
+  const [users] = await connection.execute(
+    "SELECT * FROM users WHERE username=?",
     [username]
   )
+  if(!Array.isArray(users) || users.length == 0){
+    res.status(400).send("Credenziali errate")
+    return
+  }
+
+  const user = users[0] as any
+
+  const correcrPassword = bycrypt.compare(password, user.password)
+  if(!correcrPassword){
+    res.status(400).send("Credenziali errate")
+    return
+  }
+
+  delete user.password
 
   res.json({ message: "Login effettuato con successo" })
 }
--- vue-express-auth-main/vuepost-without-auth/backend/src/utils/auth.ts
+++ vue-express-auth-main/vuepost-without-auth/backend/src/utils/auth.ts
@@ -1,4 +1,6 @@
+import strict from "assert/strict"
 import { Request, Response } from "express"
+import jwt from "jsonwebtoken"
 
 // Campi da inserire nell'access token
 export interface User {
@@ -16,16 +18,39 @@ const COOKIE_NAME = "vuepost-access-token"
  * Codifica l'utente in un access token e lo imposta come cookie.
  * Usato in fase di registrazione e login.
  */
-export const setUser = (req: Request, res: Response, user: any) => {}
+export const setUser = (req: Request, res: Response, user: any) => {
+  //crea token dal payload
+  const acessToken = jwt.sign(user, JWT_SECRET, {expiresIn: "1 day"})
+  res.cookie(COOKIE_NAME, acessToken, {
+    maxAge:86400000, //1 giorno
+    httpOnly: true,
+    sameSite: "strict",
+    //secure: true
+  })
+
+}
 
 /**
  * Decodifica e verifica l'access token, restituendo l'utente.
  * Usato per verificare se l'utente ha effettuato il login.
  */
-export const getUser = (req: Request, res: Response) => {}
+export const getUser = (req: Request, res: Response) => {
+  const accessToken = req.cookies[COOKIE_NAME]
+  
+  if(!accessToken) return null
+
+
+  try{
+  const user = jwt.verify(accessToken, JWT_SECRET) as User
+  } catch{
+    return null
+  }
+}
 
 /**
  * Cancella il cookie contente l'access token.
  * Usato per effettuare il logout.
  */
-export const unsetUser = (req: Request, res: Response) => {}
+export const unsetUser = (req: Request, res: Response) => {
+  res.clearCookie(COOKIE_NAME)
+}
